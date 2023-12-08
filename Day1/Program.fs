9efefdeb93cd002d3a37d38710e067b28086bd9f
Ercan Tomaç <ercan2243ercan@gmail.com>
2023-12-08 18:59:32 +0100

    day 1 and day 2 solutions

---
+++ Day1/Program.fs
@@ -0,0 +1,27 @@
+﻿open System.IO
+
+let lines = File.ReadAllLines(@"C:\Users\fb_er\F#\Advent of Code\Day1\input.txt")
+
+// Convert file lines into a list.
+let list = Seq.toList lines
+
+let mutable result = 0
+
+for line in list do
+    printfn $"{line}"
+    let mutable sum = -1
+    let mutable lastNum = 0
+    for character in line do
+        let isNumber = System.Char.IsNumber(character)
+
+        if isNumber then
+            let digit = System.Char.GetNumericValue(character) |> int
+            if sum = -1 then
+                sum <- (digit * 10)
+            lastNum <- digit
+
+    sum <- (sum + lastNum)
+    printfn $"{sum}"
+    result <- (result + sum)
+
+printfn $"{result}"
---
+++ Day2/Program.fs
@@ -0,0 +1,59 @@
+﻿open System.IO
+
+let lines = File.ReadAllLines(@"C:\Users\fb_er\F#\Advent of Code\Day2\input.txt")
+
+// Convert file lines into a list.
+let list = Seq.toList lines
+let mutable result = 0
+
+for i in 0 .. list.Length - 1 do
+    let line = list[i]
+    printfn $"{line}"
+
+    let j = line.IndexOf(':')
+
+    let gameNo = int ((line[ .. (j - 1) ].Split ' ')[1])
+    printfn $"{gameNo}"
+    let gameSets = line[ (j + 2) .. ].Split ';'
+    let mutable gameFailed = false
+
+    for set in gameSets do
+        if gameFailed = false then
+            let setTrimmed = set |> String.filter (fun c -> c <> ' ')
+            printfn $"{setTrimmed}"
+            let individualCubes = setTrimmed.Split ','
+
+            for cube in individualCubes do
+                if gameFailed = false then
+                    //printfn $"{cube}"
+                    let mutable breakLoop = false
+                    let mutable color = ""
+                    let mutable quantity = "-1"
+
+                    for k in 0 .. cube.Length - 1 do
+                        if breakLoop = false then
+                            let isLetter = (System.Char.IsNumber(cube[k])) = false
+
+                            if isLetter then
+                                color <- cube[k..]
+                                quantity <- cube[.. (k - 1)]
+                                breakLoop <- true
+
+                    let quantityInt = int quantity
+
+                    if color = "red" && quantityInt > 12 then
+                        printfn "red failed"
+                        gameFailed <- true
+
+                    elif color = "green" && quantityInt > 13 then
+                        printfn "green failed"
+                        gameFailed <- true
+
+                    elif color = "blue" && quantityInt > 14 then
+                        printfn "blue failed"
+                        gameFailed <- true
+
+    if gameFailed = false then
+        result <- (result + gameNo)
+
+printfn $"{result}"
