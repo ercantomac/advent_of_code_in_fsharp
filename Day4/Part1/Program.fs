873e913a473149ae31bf249516480dc8c54b1f7d
Ercan Tomaç <ercan2243ercan@gmail.com>
2023-12-16 16:56:47 +0100

    day 4 - part 1 and part 2 solutions

---
+++ Day4/Part1/Program.fs
@@ -0,0 +1,44 @@
+﻿open System.IO
+
+let lines = File.ReadAllLines(@"C:\Users\fb_er\F#\Advent of Code\Day4\Part1\input.txt")
+
+// Convert file lines into a list.
+let list = Seq.toList lines
+
+let pow (number: int) (power: int) : int =
+    if power < 0 then
+        0
+    elif power = 0 then
+        1
+    else
+        let mutable result = 1
+
+        for i in 1..power do
+            result <- (result * number)
+
+        result
+
+let mutable resultSum = 0
+
+for line in list do
+    let lineSplitted = line.Split '|'
+    let winningNumbersString = (lineSplitted[ 0 ].Split ':')[1]
+    let myNumbersString = lineSplitted[1]
+
+    let winningNumbers =
+        winningNumbersString.Split ' '
+        |> Array.filter (fun a -> a.Length > 0)
+
+    let myNumbers =
+        myNumbersString.Split ' '
+        |> Array.filter (fun a -> a.Length > 0)
+
+    let mutable numberOfMatches = 0
+
+    for winningNumber in winningNumbers do
+        if Array.contains winningNumber myNumbers then
+            numberOfMatches <- (numberOfMatches + 1)
+
+    resultSum <- (resultSum + (pow 2 (numberOfMatches - 1)))
+
+printfn $"{resultSum}"
---
+++ Day4/Part2/Program.fs
@@ -0,0 +1,52 @@
+﻿open System.IO
+
+let lines =
+    File.ReadAllLines(@"C:\Users\fb_er\F#\Advent of Code\Day4\Part2\input.txt")
+
+// Convert file lines into a list.
+let list = Seq.toList lines
+
+let mutable resultSum = 0
+let mutable associativeMap: Map<int, int list> = Map []
+
+for lineIndex in 0 .. list.Length - 1 do
+    let line = list[lineIndex]
+    let lineSplitted = line.Split '|'
+    let winningNumbersString = (lineSplitted[ 0 ].Split ':')[1]
+    let myNumbersString = lineSplitted[1]
+
+    let winningNumbers =
+        winningNumbersString.Split ' '
+        |> Array.filter (fun a -> a.Length > 0)
+
+    let myNumbers =
+        myNumbersString.Split ' '
+        |> Array.filter (fun a -> a.Length > 0)
+
+    let mutable numberOfMatches = 0
+
+    for winningNumber in winningNumbers do
+        if Array.contains winningNumber myNumbers then
+            numberOfMatches <- (numberOfMatches + 1)
+
+    associativeMap <- (Map.add (lineIndex + 1) [ 1; numberOfMatches ] associativeMap)
+
+for key in associativeMap.Keys do
+    let cardQuantity = associativeMap[key][0]
+    let numberOfMatches = associativeMap[key][1]
+
+    for i in 1..numberOfMatches do
+        if Map.containsKey (key + i) associativeMap then
+            let tempValue = associativeMap[(key + i)]
+
+            associativeMap <-
+                (Map.add
+                    (key + i)
+                    [ (tempValue[0] + cardQuantity)
+                      tempValue[1] ]
+                    associativeMap)
+
+for mapEntry in associativeMap do
+    resultSum <- (resultSum + mapEntry.Value[0])
+
+printfn $"{resultSum}"
